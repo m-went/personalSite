1a8707408a9af40dc1b2a0b7f1835b4b0d43edab
Michał Went <wentmichal@gmail.com>
2022-11-23 19:31:42 +0100

    add basic aboutMe component layout

--- src/components/organisms/AboutMe/AboutMe.js
+++ src/components/organisms/AboutMe/AboutMe.js
@@ -1,11 +1,21 @@
 import styles from './AboutMe.module.scss';
+import photo from '../../../assets/imgs/Red.jpg';
 
 function AboutMe(props) {
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur tempora quae quos, doloribus cum atque
-        perspiciatis sed omnis rem ut consequuntur fuga, eaque fugiat ex molestias! Fugiat inventore labore porro?{' '}
+    <div className={`${styles.aboutMe}`}>
+      <h2 className={`${styles.aboutTitle}`}>A few words about me...</h2>
+      <img className={`${styles.aboutPhoto}`} src={photo} alt="Yup, it's still me" />
+      <p className={`${styles.aboutText}`}>
+        Hello! As You probably know, my name is Michał. I'm from Poland and actually living in Rumia, near Tricity. I
+        hope, that now i'm a nice pretender to become junior front-end developer and You're reading this means, that my
+        dream can come true. I started learning web development in september 2021 and came a long way to learn what I
+        know now, but i also know that my knowlege is like a raindrop in the sea. Fortunatelly I also learn a lot about
+        myself and can guarantee, that I'll keep learning to become better and better. So, that's about my learning
+        history, time for more personall stuff &#59;&#41; I completed economy studies at Gdańsk University, found
+        beautiful and smart wive and after a few years as fire protection technician decided, that I want more
+        opportunities to learn new thing and started learning programming. Also I have to admit, that I'm a big fan of
+        boardgames and maybe a little fan of dad jokes :D
       </p>
     </div>
   );
