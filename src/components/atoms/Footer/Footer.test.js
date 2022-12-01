ae6009a75a7404e77e0601e659f3298d3f2b0d1b
Michał Went <wentmichal@gmail.com>
2022-12-01 21:01:11 +0100

    Updates

--- src/components/atoms/Footer/Footer.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Footer from './Footer';
-
-test('render footer component', () => {
-  render(<Footer />);
-  const footerText = screen.getByText(/Michał Went/);
-  expect(footerText).toBeInTheDocument();
-});
