87481cc5434a6dfb5d020b726e2a2b452f5be3bf
Michał Went <wentmichal@gmail.com>
2022-12-03 16:10:30 +0100

    add scroll animation to arrow component on click

--- src/components/atoms/Arrow/Arrow.js
+++ src/components/atoms/Arrow/Arrow.js
@@ -3,12 +3,21 @@ import styles from './Arrow.module.scss';
 
 function Arrow(props) {
   return (
-    <a href="#about-me" className={`${styles.arrowContainer}`}>
+    <div
+      onClick={() => {
+        window.scroll({
+          top: window.innerHeight - 50,
+          left: 0,
+          behavior: 'smooth',
+        });
+      }}
+      className={`${styles.arrowContainer}`}
+    >
       <ArrowDown className={`${styles.arrow}`} />
       <div className={`${styles.circle}`} style={{ animationDelay: '0s' }} />
       <div className={`${styles.circle}`} style={{ animationDelay: '1s' }} />
       <div className={`${styles.circle}`} style={{ animationDelay: '2s' }} />
-    </a>
+    </div>
   );
 }
 
