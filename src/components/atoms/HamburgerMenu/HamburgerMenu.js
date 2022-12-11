cabae2a2f0562194c4cd5852e60f49141e0ccab0
Michał Went <wentmichal@gmail.com>
2022-12-11 14:10:46 +0100

    delete default react files and prepare for deploy

--- src/components/atoms/HamburgerMenu/HamburgerMenu.js
+++ src/components/atoms/HamburgerMenu/HamburgerMenu.js
@@ -1,6 +1,27 @@
 import styles from './HamburgerMenu.module.scss';
 
 function HamburgerMenu({ isOpen, toggleBurger }) {
+  const mobileNavbar = (
+    <nav className={`${styles.mobileNavbar}`}>
+      <ul className={`${styles.mobileNavList}`}>
+        <a className={`${styles.mobileNavSection}`} href="#home">
+          <li>Home</li>
+        </a>
+        <a className={`${styles.mobileNavSection}`} href="#about-me">
+          <li>About me</li>
+        </a>
+        <a className={`${styles.mobileNavSection}`} href="#skills">
+          <li>Skills</li>
+        </a>
+        <a className={`${styles.mobileNavSection}`} href="#projects">
+          <li>Projects</li>
+        </a>
+        <a className={`${styles.mobileNavSection}`} href="#contact">
+          <li>Contact</li>
+        </a>
+      </ul>
+    </nav>
+  );
   return (
     <div className={`${styles.hamburgerMenu}`} onClick={toggleBurger}>
       <div className={`${styles.burgerlines}  ${isOpen ? styles.isOpen : ''}`}>
@@ -8,6 +29,7 @@ function HamburgerMenu({ isOpen, toggleBurger }) {
         <div className={`${styles.line} ${styles.mid} `}></div>
         <div className={`${styles.line} ${styles.bot} `}></div>
       </div>
+      {isOpen ? mobileNavbar : null}
     </div>
   );
 }
